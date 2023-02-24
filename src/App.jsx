80e275bbc9ce110fb9d053a140410e1dc9e7071a
HeitorPontieri <heitorpontieri1@gmail.com>
2023-02-24 11:24:52 -0300

    hahah

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import { Navbar } from './Components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+       <div className='navbar-container'>
+          <Navbar/>
+       </div>
+      </div> 
+  );
+}
+
+export default App;
